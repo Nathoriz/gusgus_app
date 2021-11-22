dc2cdfad70916852cc9c24fbc1b2d37b51e22037
Mod <natalyortiz.401@gmail.com>
2021-11-22 18:45:31 -0500

    Login y Registro

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/signup.kt
@@ -0,0 +1,18 @@
+package mood.honeyprojects.gusgusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mood.honeyprojects.gusgusapp.databinding.ActivitySignupBinding
+
+class signup : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
