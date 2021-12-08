7b512c30d581c290e50fd3cb45221a3f23db049a
DeamBrose <emiliodragon98@hotmail.com>
2021-12-08 18:51:54 -0500

    Update App version 1.4

--- app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/AdminMainActivity.kt
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/AdminMainActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import mood.honeyprojects.gusgusapp.R
 import mood.honeyprojects.gusgusapp.databinding.ActivityAdminMainBinding
 import mood.honeyprojects.gusgusapp.sharedPreferences.Preferences
 import java.text.DateFormat
@@ -25,6 +30,7 @@ class AdminMainActivity : AppCompatActivity() {
         ShowUserAdmin()
         MostrarHora()
         Listener()
+        animation()
     }
     private fun Listener(){
         binding.txtlogout.setOnClickListener { LogOut() }
@@ -60,5 +66,49 @@ class AdminMainActivity : AppCompatActivity() {
         Preferences.constantes.saveTelefono( "" )
         startActivity( Intent( applicationContext, LoginActivity::class.java ) )
         finish()
+    }
+    private fun animation(){
+        val scaleup = AnimationUtils.loadAnimation( this, R.anim.scale_up )
+        val scaledown = AnimationUtils.loadAnimation( this, R.anim.scale_down )
+        binding.cvProducto.setOnTouchListener( object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if( event?.action == MotionEvent.ACTION_UP ){
+                    binding.cvProducto.startAnimation( scaleup )
+                }else if( event?.action == MotionEvent.ACTION_DOWN ){
+                    binding.cvProducto.startAnimation( scaledown )
+                }
+                return true
+            }
+        } )
+        binding.cvPedidos.setOnTouchListener( object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if( event?.action == MotionEvent.ACTION_UP ){
+                    binding.cvPedidos.startAnimation( scaleup )
+                }else if( event?.action == MotionEvent.ACTION_DOWN ){
+                    binding.cvPedidos.startAnimation( scaledown )
+                }
+                return true
+            }
+        } )
+        binding.cvPublicidad.setOnTouchListener( object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if( event?.action == MotionEvent.ACTION_UP ){
+                    binding.cvPublicidad.startAnimation( scaleup )
+                }else if( event?.action == MotionEvent.ACTION_DOWN ){
+                    binding.cvPublicidad.startAnimation( scaledown )
+                }
+                return true
+            }
+        } )
+        binding.cvUsuarios.setOnTouchListener( object: View.OnTouchListener{
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if( event?.action == MotionEvent.ACTION_UP ){
+                    binding.cvUsuarios.startAnimation( scaleup )
+                }else if( event?.action == MotionEvent.ACTION_DOWN ){
+                    binding.cvUsuarios.startAnimation( scaledown )
+                }
+                return true
+            }
+        } )
     }
 }
