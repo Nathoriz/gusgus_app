df93f32d65a1292c92880e96b7d0f080063dd3b5
DeamBrose <emiliodragon98@hotmail.com>
2021-12-09 20:01:38 -0500

    Update App version 1.4.1

--- app/src/main/java/mood/honeyprojects/gusgusapp/view/adapter/ProductoViewHolder.kt
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/adapter/ProductoViewHolder.kt
@@ -5,12 +5,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import mood.honeyprojects.gusgusapp.databinding.ItemCardProductoBinding
 import mood.honeyprojects.gusgusapp.model.entity.Producto
+import java.lang.Exception
 
 class ProductoViewHolder( view: View ): RecyclerView.ViewHolder( view ) {
     private val binding =ItemCardProductoBinding.bind( view )
 
     fun bind( producto: Producto ){
+        binding.txterror.visibility = View.GONE
         binding.txtNombreTorta.text = producto.nombre
-        Picasso.get().load( producto.urlimg ).into( binding.ivtorta )
+        Picasso.get().load( producto.urlimg ).into( binding.ivtorta, object: com.squareup.picasso.Callback{
+            override fun onSuccess() {
+                binding.progressBar.visibility = View.GONE
+                binding.txterror.visibility = View.GONE
+                binding.ivtorta.visibility = View.VISIBLE
+                binding.txtNombreTorta.visibility = View.VISIBLE
+            }
+
+            override fun onError(e: Exception?) {
+                binding.progressBar.visibility = View.GONE
+                binding.txterror.text = "No se puede cargar\nla imagen. \uD83D\uDE14"
+                binding.txterror.visibility = View.VISIBLE
+                binding.ivtorta.visibility = View.INVISIBLE
+                binding.txtNombreTorta.visibility = View.VISIBLE
+            }
+        } )
     }
 }
