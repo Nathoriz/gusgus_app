313ad64d55d785a22e19a25a50a9000439f77b59
Mod <natalyortiz.401@gmail.com>
2021-12-13 17:39:15 -0500

    Diseño realizar pedido

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/ConfirmOrderActivity.kt
@@ -0,0 +1,11 @@
+package mood.honeyprojects.gusgusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_order)
+    }
+}
