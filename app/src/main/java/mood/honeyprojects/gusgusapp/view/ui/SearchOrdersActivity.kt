c4b189343d315e30dd96b445dccc8c591e7f72cf
Mod <natalyortiz.401@gmail.com>
2021-12-21 14:57:31 -0500

    Style Title - Subtitle, Diseño SearchOrder, TortaPersonalizada

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/SearchOrdersActivity.kt
@@ -0,0 +1,12 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mood.honeyprojects.gusgusapp.R
+
+class SearchOrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_orders)
+    }
+}
