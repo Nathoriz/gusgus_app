3ddb3b53d8f598d109bd23e9955d917dd100ab22
Mod <natalyortiz.401@gmail.com>
2021-12-23 17:51:40 -0500

    Primer parte Diseño Detalle Pedido Personalización

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/DetailTortaPersonalizadaActivity.kt
@@ -0,0 +1,12 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mood.honeyprojects.gusgusapp.R
+
+class DetailTortaPersonalizadaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_torta_personalizada)
+    }
+}
--- app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/TortasPersonalizadasActivity.kt
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/TortasPersonalizadasActivity.kt
@@ -1,7 +1,8 @@
-package mood.honeyprojects.gusgusapp
+package mood.honeyprojects.gusgusapp.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mood.honeyprojects.gusgusapp.R
 
 class TortasPersonalizadasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
