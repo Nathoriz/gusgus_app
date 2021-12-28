2746606a7f25409e5cd84288b9ab7762aa7561a8
Mod <natalyortiz.401@gmail.com>
2021-12-28 15:03:40 -0500

    Mant Noticia

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/AddNoticiaActivity.kt
@@ -0,0 +1,11 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoticiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_noticia)
+    }
+}
---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/NoticiaActivity.kt
@@ -0,0 +1,11 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoticiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_noticia)
+    }
+}
