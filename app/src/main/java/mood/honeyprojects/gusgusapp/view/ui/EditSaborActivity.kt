d51a4462c8968411826b10bcaac33db8b5125efd
Mod <natalyortiz.401@gmail.com>
2021-12-30 05:16:38 -0500

    edit sabor - click btn cambiar inputype:none por text & editable:true

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/EditSaborActivity.kt
@@ -0,0 +1,12 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mood.honeyprojects.gusgusapp.R
+
+class EditSaborActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_sabor)
+    }
+}
