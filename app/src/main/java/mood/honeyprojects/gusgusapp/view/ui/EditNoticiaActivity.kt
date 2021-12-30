87800a7e4525926c4f4e22368b57787344d80d76
Mod <natalyortiz.401@gmail.com>
2021-12-30 04:40:07 -0500

    activity editnoticia

---
+++ app/src/main/java/mood/honeyprojects/gusgusapp/view/ui/EditNoticiaActivity.kt
@@ -0,0 +1,11 @@
+package mood.honeyprojects.gusgusapp.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNoticiaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_noticia)
+    }
+}
